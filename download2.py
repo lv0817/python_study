0a24f7e41f350213cd4c56e75be18334c2ae7920
lv0817 <412435484@qq.com>
2018-01-29 10:35:55 +0800

    爬取百度贴吧中的图片

---
+++ download2.py
@@ -0,0 +1,23 @@
+import urllib.request
+import re
+
+def open_url(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent','ozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36')
+    page = urllib.request.urlopen(req)
+    html = page.read().decode('utf-8')
+
+    return html
+
+def get_img(html):
+    '''从给定的网页中利用正则表达式匹配对象'''
+    p = r'<img class="BDE_Image" src="[^"]+\.jpg'
+    imglist = re.findall(p,html)
+
+    for each in imglist:
+        print(each)
+
+
+if __name__ == '__main__':
+    url = 'https://tieba.baidu.com/p/5393582905'
+    get_img(open_url(url))
